21e7f316f0ea76e4aacdfa4db0dd76136d80d6bb
Parsa Alpha <alpha@parsa-alpha.cf>
2016-12-24 19:00:40 +0330

    Update self-bot.lua

--- bot/self-bot.lua
+++ bot/self-bot.lua
@@ -52,13 +52,6 @@ function on_binlog_replay_end()
   load_plugins()
 end
 
-function msg_valid(msg)
-  -- Don't process outgoing messages
-  if msg.out then
-    print('\27[36mNot valid: msg from us\27[39m')
-    return true
-  end
-
   -- Before bot was started
   if msg.date < os.time() - 5 then
     print('\27[36mNot valid: old msg\27[39m')
@@ -80,10 +73,6 @@ function msg_valid(msg)
     return false
   end
 
-  if msg.from.id == our_id then
-    print('\27[36mNot valid: Msg from our id\27[39m')
-    return true
-  end
 
   if msg.to.type == 'encr_chat' then
     print('\27[36mNot valid: Encrypted chat\27[39m')
